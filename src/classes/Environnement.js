1a5a73456c90fec44f02f0a9592d31711de72dc5
Craaftx <guilmain.dorian@gmail.com>
2019-06-11 20:01:45 +0200

    :construction: Create item placement

--- src/classes/Environnement.js
+++ src/classes/Environnement.js
@@ -1,13 +1,15 @@
 export class Environnement {
-    /**
-     * Represents the map environnement.
-     * @constructor
-     * @param {object} size - The size of the map.
-     * @param {integer} size - The size of the map.
-     */
-  constructor(map, blocks) {
+  /**
+   * Represents the map environnement.
+   * @constructor
+   * @param {object} map - The map object.
+   * @param {array} block - The game blocks.
+   * @param {array} items - The game items.
+   */
+  constructor(map, blocks, items) {
     this.map = map;
     this.blocks = blocks;
+    this.items = items;
     this.composition = [];
   }
 
@@ -33,7 +35,8 @@ export class Environnement {
    * @param {bool} [time=500] - Max time for generate the map, used by animation.
    */
   buildMap(mapPattern = null, time = 500) {
-    let usableBlocks = this.composition.length == 0 ? this.blocks : this.composition;
+    let usableBlocks =
+      this.composition.length == 0 ? this.blocks : this.composition;
     let animationDelay = 0;
     let animationIncrement = time / this.map.size;
     for (let row = 0; row < this.map.size; row++) {
@@ -46,13 +49,13 @@ export class Environnement {
         newDiv.setAttribute("class", "gamegrid__item__content");
 
         let block;
-        if(mapPattern === null) {
-            block =
+        if (mapPattern === null) {
+          block =
             usableBlocks[
               Math.floor(Math.random() * Math.floor(usableBlocks.length))
             ];
         } else {
-            block = mapPattern.getBlockInCell(row,col);
+          block = mapPattern.getBlockInCell(row, col);
         }
 
         this.map.addBlockToCell(row, col, block);
@@ -73,6 +76,44 @@ export class Environnement {
         currentItem.appendChild(newDiv);
 
         animationDelay += animationIncrement;
+      }
+    }
+  }
+
+  /**
+   * Populate the grid with items and add to virtual grid the items properties.
+   * @param {bool} [delay=500] - Delay for generate the items, used by animation.
+   */
+  placeItems(number, delay = 500) {
+    var currentGameItems = 0;
+    for (var i = 0; i < virtualGrid.length; i++) {
+      for (var x = 0; x < virtualGrid.length; x++) {
+        var currentBlock = document.getElementById(
+          "gamegrid__item-" + i + "-" + x
+        );
+        if (
+          currentGameItems < maxNumber &&
+          variation - 1 == getRandomInt(variation) &&
+          !isAirBlock(currentBlock)
+        ) {
+          var currentItem = currentBlock.getElementsByClassName(
+            "gamegrid__item__content"
+          )[0];
+          var newBlock = document.createElement("img");
+          newBlock.setAttribute(
+            "src",
+            "../game_assets/ground-items/" +
+              gameItems[getRandomInt(gameItems.length)] +
+              ".png"
+          );
+          newBlock.setAttribute("class", "gamegrid__item__content--gameitem");
+          newBlock.setAttribute(
+            "style",
+            "animation-delay:" + (globalBlockGenerationTime + 40) + "ms"
+          );
+          currentItem.appendChild(newBlock);
+          currentGameItems++;
+        }
       }
     }
   }
