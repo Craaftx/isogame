396e4ff7d65d05beef12cf3205e484af2d065740
Craaftx <guilmain.dorian@gmail.com>
2019-07-16 16:34:40 +0200

    :sparkles: Add player sprite generation

---
+++ src/classes/Sprite.js
@@ -0,0 +1,44 @@
+export class Sprite {
+    /**
+     * Represents a sprite.
+     * @constructor
+     * @param {string} name - Identifier name.
+     * @param {integer} spriteWidth - Width of a frame sprite.
+     * @param {integer} spriteHeight - Height of a frame sprite.
+     * @param {integer} frameSpeed - Frame speed in seconds for animation speed
+     * @param {integer} frameNumber - Number of frame for the animation
+     * @param {string} frameMapUrl - URL of the frameMap.
+     * @param {bool} [inverse=false] - Set to true to inverse the sprite on the X axe.
+     */
+    constructor(name, level, spriteWidth, spriteHeight, frameSpeed, frameNumber, frameMapUrl, inverse = false) {
+        this._name = name;
+        this._level = level;
+        this._inverse = inverse;
+        this._spriteWidth = spriteWidth;
+        this._spriteHeight = spriteHeight;
+        this._frameSpeed = frameSpeed;
+        this._frameNumber = frameNumber;
+        this._frameMapUrl = frameMapUrl;
+        this._inverse = inverse;
+    }
+
+    buildSprite($parentId) {
+      let $parent = document.querySelector(`#${$parentId}`);
+      let $newSpriteContainer = document.createElement("div");
+      $newSpriteContainer.setAttribute("class", "players__player");
+      let $newSprite = document.createElement("div");
+      $newSprite.setAttribute("id", "players__player__sprite-" + this._name);
+      $newSprite.setAttribute("class", 
+      `players__player__sprite 
+        ${this._inverse ? "--sprite-inverse" : ""} 
+        --sprite-level-${this._level}`);
+      $newSprite.style.cssText = `
+        background: url('game_assets/monsters/${this._frameMapUrl}') no-repeat top left; 
+        width: ${this._spriteWidth}px;
+        height: ${this._spriteHeight}px; 
+        animation: animate_sprite_${this._frameNumber}_level_${this._level} ${this._frameSpeed}s steps(${this._frameNumber}, end) infinite;
+      `;
+      $newSpriteContainer.appendChild($newSprite);
+      $parent.appendChild($newSpriteContainer);
+    }
+}
