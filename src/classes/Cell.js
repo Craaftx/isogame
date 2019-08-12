cf86bfcb7c0573baf67cb6fbd15ceaedbd8b3290
Craaftx <guilmain.dorian@gmail.com>
2019-08-12 17:34:51 +0200

    :art: Rewrite Cell class

--- src/classes/Cell.js
+++ src/classes/Cell.js
@@ -8,15 +8,48 @@ export default class Cell {
      * @param {object} rightCell - The adjacent cell on the right.
      * @param {integer} xAxis - x position on the map.
      * @param {integer} yAxis - y position on the map.
-     * @param {bool} reachable - The cell is reachable.
+     * @param {object} block - The block type of the cell.
+     * @param {object} contains - The contains of the cell (items, characters).
+     * @param {bool} isReachable - The cell is reachable.
      */
-    constructor(topCell, bottomCell, leftCell, rightCell, xAxis, yAxis, block, reachable, item) {
+    constructor(topCell, bottomCell, leftCell, rightCell, xAxis, yAxis, block, contains = null, isReachable = true) {
       this._topCell = topCell;
       this._bottomCell = bottomCell;
       this._leftCell = leftCell;
       this._rightCell = rightCell;
       this._xAxis = xAxis;
       this._yAxis = yAxis;
+      this._block = block;
+      this._contains = contains;
+      this._isReachable = isReachable;
+    }
+
+    get topCell() {
+      return this._topCell;
+    }
+
+    get bottomCell() {
+      return this._bottomCell;
+    }
+
+    get leftCell() {
+      return this._leftCell;
+    }
+
+    get rightCell() {
+      return this._rightCell;
+    }
+
+    get xAxis() {
+      return this._xAxis;
+    }
+
+    get yAxis() {
+      return this._yAxis;
+    }
+
+    get isReachable() {
+      return this._isReachable;
     }
   }
   
