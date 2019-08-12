7aad669395292d2806a874f15277b534324f55f5
Craaftx <guilmain.dorian@gmail.com>
2019-08-12 18:43:01 +0200

    :lipstick: Add Cell selection on Grid

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import "./main.scss";
 import Block from "./classes/Block";
 import Item from "./classes/Item";
+import Character from "./classes/Character";
 import Properties from "./classes/Properties";
 import Map from "./classes/Map";
 import Pattern from "./classes/Pattern";
@@ -49,7 +50,7 @@ let items = {
     new Item("spell_fire_superior", "Livre de pyromancie supérieur", "A utiliser avec précaution !", new Properties(15, -5, 0, 3)),
     new Item("spell_bones_minor", "Livre de nécromancie mineur", "A utiliser avec précaution !", new Properties(0, 10, 0, 1)),
     new Item("spell_fire_superior", "Livre de pyromancie supérieur", "A utiliser avec précaution !", new Properties(15, -5, 0, 3)),
-    new Item("spell_bones_minor", "Livre de nécromancie mineur", "A utiliser avec précaution !", new Properties(0, 10, 0, 1)),
+    new Item("spell_bones_minor", "Livre de nécromancie mineur", "A utiliser avec précaution !", new Properties(0, 20, 0, 1)),
   ],
 }
 
@@ -122,7 +123,6 @@ let riverBlocks = [
 let size = 12;
 
 let map = new Map(size);
-console.table(map.getVirtualMap());
 map.generateGrid("gamegrid");
 
 let lackPattern = new Pattern(lack, lackBlocks);
@@ -138,3 +138,14 @@ environnement.placeItems(4);
 level_1_idle_001.buildSprite('players');
 level_2_walk_001.buildSprite('players');
 level_3_walk_001.buildSprite('players');
+
+let player1 = new Character("monster1", "Big Tooth", "Monstre très méchant", new Properties(10, 5, 20, 1), [level_1_idle_001]);
+
+console.log(player1.defensePower());
+console.log(player1.item);
+console.log('----------------------------------');
+
+player1.item = items.books[5];
+
+console.log(player1.defensePower());
+console.log(player1.item);
